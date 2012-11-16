4916661a587ea008712149bc7411adac73131f1a
tmr232 <tmr232@github>
2012-11-16 16:36:58 +0200

    Some progress. How I hare JS function binding...

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-var currentTime = 0;
-var interval = 100; //milliseconds
-var timerIntervalObject;
-
 /**
  * Returns the current time
  * @returns {Number} Returns the current time in milliseconds.
@@ -22,7 +18,7 @@ function Task(callback, timeout, description) {
     
     if (arguments.length === 3) {
         this.description = description;
-    }
+    };
 }
 
 /**
@@ -38,7 +34,7 @@ function Scheduler(tasks, resolution) {
     this.tasks = tasks.sort(compareTask);
     this.taskIndex = 0;
     this.resolution = resolution;
-    this.interval = none;
+    this.interval = null;
     this.currentTime = 0;
     
     this.addTask = function(task) {
@@ -47,12 +43,20 @@ function Scheduler(tasks, resolution) {
     };
     
     this.start = function() {
+        if (this.interval === null) {
         this.interval = setInterval(this.timingEvent, this.resolution);
+        }
     };
     
     this.stop = function() {
         clearInterval(this.interval);
-        this.interval = none;
+        this.interval = null;
+    };
+    
+    this.reset = function() {
+        this.stop();
+        this.setCurrentTime = 0;
+        this.taskIndex = 0;
     };
     
     this.setCurrentTime = function(currentTime) {
@@ -63,21 +67,40 @@ function Scheduler(tasks, resolution) {
         return this.currentTime;
     };
     
+    var self = this;
     this.timingEvent = function() {
-        this.currentTime += this.resolution;
+        self.currentTime += self.resolution;
+        for (; (self.taskIndex < self.tasks.length) && (self.tasks[self.taskIndex].timeout <= self.currentTime); ++self.taskIndex) {
+            self.tasks[self.taskIndex].callback();
+        }
         
-        for (; this.tasks[this.taskIndex] <= this.currentTime; ++this.taskIndex) {
-            this.tasks[this.taskIndex].callback();
+        if (self.taskIndex >= self.tasks.length) {
+            self.stop();
         }
     };
 }
 
-function timerFunction() {
-    currentTime += interval;
-    var display = document.querySelector("#display");
-    display.innerText = currentTime;
+function createTextSetter(id, text) {
+    function textSetter() {
+        document.querySelector(id).innerText = text;
+    };
+    return textSetter;
 }
 
 function init() {
-    timerIntervalObject = setInterval(timerFunction, interval);
+    var tasks = [
+        new Task(createTextSetter("#display", "first"), 0),
+        new Task(createTextSetter("#display", "second"), 2000),
+        new Task(createTextSetter("#display", "third"), 4000),
+        new Task(createTextSetter("#display", "fourth"), 6000),
+        new Task(createTextSetter("#display", "fifth"), 8000),
+        new Task(createTextSetter("#display", "sixth"), 10000),
+        new Task(createTextSetter("#display", "seventh"), 12000)
+    ];
+    
+    var sched = new Scheduler(tasks, 500);
+    
+    document.querySelector("#start").onclick = sched.start.bind(sched);
+    document.querySelector("#stop").onclick = sched.stop.bind(sched);
+    document.querySelector("#reset").onclick = sched.reset.bind(sched);
 }
